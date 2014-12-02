ac19ee1dff0860763e12d99134f41185917645cb
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-12-02 16:28:23 -0500

    HEEELP

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
-	startAnimations();
-	setInterval(updateTime, 1000);
-	requestAnimationFrame(updateEpoch);
-	registerKeyEvents();
-});
-
-function startAnimations() {
 	window.scroll(0, 0);
 	$(".main-content").hide();
 	$(".slide-up").css({height: $(window).height() - $(".header-box").height()});
-	$("#hexInput").val("#000000")
-	$("#rgbInput").val("0,0,0");
 	headerContent.applyTo($(".header-content"));
 	$(".header-content").animate({opacity: 1});
 	setTimeout(function() {
@@ -22,40 +13,18 @@ function startAnimations() {
 		$(".main-content").fadeIn();
 		$("body").css({"overflow": "visible"});
 	}, 3000);
-}
 
-function updateTime() {
-	var local = new Date();
-    var h = local.getHours();
-    var m = local.getMinutes();
-    var s = local.getSeconds();
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-    var time = h + ":" + m + ":" + s;
-    $(".time-local").html(time);
-    $(".time-local-12-hour").html(h > 12 ? (h - 12) + ":" + m + ":" + s : time);
-}
+	setInterval(function() {
+		var local = new Date();
+	    var h = local.getHours();
+	    var m = local.getMinutes();
+	    var s = local.getSeconds();
+	    m = m < 10 ? "0" + m : m;
+	    s = s < 10 ? "0" + s : s;
+	    var time = h + ":" + m + ":" + s;
+	    $(".time-local").html(time);
+	    $(".time-local-12-hour").html(h > 12 ? (h - 12) + ":" + m + ":" + s : time);
+	}, 1000);
 
-function updateEpoch() {
-	$(".time-epoch").text(Date.now());
-	requestAnimationFrame(updateEpoch);
-}
+});
 
-function registerKeyEvents() {
-	$("#rgbInput").keyup(function() {
-		var rgb = getRGB();
-		var hex;
-		if (rgb) {
-			rgb = rgb.split(",");
-			hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
-			$("#hexInput").val(hex);
-			updateWithHex(hex);	
-		}
-	});
-
-	$("#hexInput").keyup(function() {
-		var hex = getHex();
-		$("#rgbInput").val(hexToRGB(hex));
-		updateWithHex(hex);
-	});
-}
